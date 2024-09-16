dbe176de639f1f327313a19d2e57243f36ce8c74
Estifo <135218620+EstifanosChekaFaltamo@users.noreply.github.com>
2024-09-17 00:22:55 +0300

    Header and Footer Done

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../Compponents/Header/Header'
+import Footer from '../../Compponents/Footer/Footer'
+
+const Home = () => {
+    return (<>
+        <Header />
+        <Footer />
+    </>)
+}
+
+export default Home
