639b3cdbcddc9eb9f8d5ab89c78caa2a2280e4f8
Estifo <135218620+EstifanosChekaFaltamo@users.noreply.github.com>
2024-09-17 23:02:33 +0300

    Banner Added

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../../Compponents/Header/Header'
 import Footer from '../../Compponents/Footer/Footer'
+import Banner from '../../Compponents/Banner/Banner'
+
 
 const Home = () => {
     return (<>
         <Header />
+        <Banner />
         <Footer />
     </>)
 }
